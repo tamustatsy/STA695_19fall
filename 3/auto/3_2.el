bc3a38b575fc56be9d6857c5b21334412a59fa0c
Suya <fsu228@MDS315.local>
2019-09-11 09:28:21 -0400

    Section 3.5-3.7 added.

---
+++ 3/auto/3_2.el
@@ -0,0 +1,27 @@
+(TeX-add-style-hook
+ "3_2"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "graphicx"
+    "booktabs"
+    "amsfonts"
+    "mathrsfs"
+    "bbold"
+    "amsmath"
+    "amssymb"
+    "mathtools")
+   (TeX-add-symbols
+    "sbf")))
+
