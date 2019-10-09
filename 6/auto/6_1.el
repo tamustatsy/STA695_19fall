e87efb5731cccb182bcd0b7da34089a5076475c2
Suya <fsu228@MDS315.local>
2019-10-09 15:06:31 -0400

    Likelihood principle and marginal predictive checks.

---
+++ 6/auto/6_1.el
@@ -0,0 +1,25 @@
+(TeX-add-style-hook
+ "6_1"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "graphicx"
+    "booktabs"
+    "amsfonts"
+    "mathrsfs"
+    "bbold"
+    "amsmath"
+    "amssymb"
+    "mathtools")))
+
