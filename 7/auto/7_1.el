2e4bf148e34a792b44e36087d59e8d68102fe684
Suya <fsu228@MDS315.local>
2019-10-14 14:21:28 -0400

    Chapter 7 initiated.

---
+++ 7/auto/7_1.el
@@ -0,0 +1,33 @@
+(TeX-add-style-hook
+ "7_1"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("adjustbox" "export")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "graphicx"
+    "booktabs"
+    "amsfonts"
+    "mathrsfs"
+    "bbold"
+    "amsmath"
+    "amssymb"
+    "mathtools"
+    "adjustbox")
+   (TeX-add-symbols
+    '("widebar" 1)
+    "rel"
+    "mathaccent"
+    "mathaccentV")))
+
